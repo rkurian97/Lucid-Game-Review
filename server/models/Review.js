929eb1f238e36f130a252c5bd767b05e44731f45
rkurian <kurian.rohit1229@gmail.com>
2021-07-17 16:44:49 -0500

    added some pages

--- server/models/Review.js
+++ server/models/Review.js
@@ -10,7 +10,7 @@ const reviewSchema = new Schema(
       minlength: 1,
       maxlength: 280
     },
-    movieId: {
+    videoGameId: {
         type: String,
         required: true
     },
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,53 @@
+const { gql } = require('apollo-server-express');
 
+const typeDefs = gql`
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        friendCount: Int
+        reviews: [Review]
+        friends: [User]
+    }
+
+    type Review {
+        _id: ID
+        reviewText: String
+        createdAt: String
+        username: String
+        commentCount: Int
+        comments: [Comment]
+        rating: Int
+        recommendations: []
+        videoGameId: String
+    }
+
+    type Comment {
+        _id: ID
+        commentBody: String
+        createdAt: String
+        username: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me: User
+        users: [User]
+        user(username: String!): User
+        reviews(username: String): [Review]
+        review(_id: ID!): Review
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addReview(reviewText: String!): Review
+        addComment(reviewId: ID!, commentBody: String!): Review
+        addFriend(friendId: ID!): User
+      }
+`
