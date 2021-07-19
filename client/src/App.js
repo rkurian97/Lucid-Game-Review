99be9b9a38432a432e50a400b28dbe21c8566cf5
rkurian <kurian.rohit1229@gmail.com>
2021-07-19 10:16:16 -0500

    starter for navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,48 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Friends from "./pages/Friends";
+import FriendPosts from "./pages/FriendPosts";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+
+// import { Provider } from 'react-redux';
+// import store from './utils/GlobalState';
+
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem('id_token')
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: '/graphql',
+})
+
 function App() {
   return (
-    <div>
-      Project 3 bb
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+            <Nav/>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={SignUp} />
+              <Route exact path="/friends" component={Friends} />
+              <Route exact path="/friendposts" component={FriendPosts} />
+              <Route component={NoMatch}/>
+            </Switch>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
---
+++ client/src/pages/FriendPosts.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FriendPosts = () => {
+    return (
+        <div>
+            Friend Posts
+        </div>
+    );
+};
+
+export default FriendPosts;
